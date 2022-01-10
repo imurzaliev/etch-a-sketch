c9b6299f7ca581896ace631be95bdb1f4f003a9e
imurzaliev <imurzalievi@gmial.com>
2022-01-10 17:23:08 +0300

    Set up a hover effect

--- app.js
+++ app.js
@@ -5,6 +5,12 @@ const gridColumn = 16;
 for (i = 0; i < gridColumn * gridRow; i++) {
   const content = document.createElement("div");
   content.classList.add("content");
-  content.textContent = i + 1;
   container.appendChild(content);
 }
+
+const pixels = document.querySelectorAll(".content");
+pixels.forEach((pixel) =>
+  pixel.addEventListener("mouseover", () => {
+    pixel.classList.add("drawing");
+  })
+);
