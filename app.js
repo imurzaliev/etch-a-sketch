b21f3922a593bfcc27bb37aa9493bca9d48dc632
imurzaliev <imurzalievi@gmial.com>
2022-01-12 21:44:41 +0300

    Add blinking effect to button

--- app.js
+++ app.js
@@ -1,16 +1,30 @@
 const container = document.querySelector(".container");
+const clearButton = document.querySelector("button");
+
+clearButton.addEventListener("click", () => {
+  clearButton.classList.add("clicked");
+});
+
+clearButton.addEventListener("transitionend", () => {
+  clearButton.classList.remove("clicked");
+});
+
+const screen = document.createElement("div");
+screen.classList.add("screen");
+container.appendChild(screen);
+
 const gridRow = 16;
 const gridColumn = 16;
 
 for (i = 0; i < gridColumn * gridRow; i++) {
   const content = document.createElement("div");
   content.classList.add("content");
-  container.appendChild(content);
+  screen.appendChild(content);
 }
 
 const pixels = document.querySelectorAll(".content");
-pixels.forEach((pixel) =>
+pixels.forEach((pixel) => {
   pixel.addEventListener("mouseover", () => {
     pixel.classList.add("drawing");
-  })
-);
+  });
+});
