5974c07c7e8f5f25161970135fb2060113ed40d3
imurzaliev <imurzalievi@gmial.com>
2022-01-15 23:31:12 +0300

    Make button clean screen and let user enter size of the next grid

--- app.js
+++ app.js
@@ -1,33 +1,61 @@
 const container = document.querySelector(".container");
 const clearButton = document.querySelector("button");
-
-clearButton.addEventListener("click", () => {
-  clearButton.classList.add("clicked");
-});
-
 const screen = document.createElement("div");
 screen.classList.add("screen");
 container.appendChild(screen);
+let screenArea = 3;
 
-const gridRow = 16;
-const gridColumn = 16;
+function createScreen(area) {
+  screen.setAttribute(
+    "style",
+    `grid-template-columns: repeat(${area}, 1fr);  grid-template-rows: repeat(${area}, 1fr)`
+  );
 
-for (i = 0; i < gridColumn * gridRow; i++) {
-  const content = document.createElement("div");
-  content.classList.add("content");
-  screen.appendChild(content);
+  for (i = 0; i < area * area; i++) {
+    const content = document.createElement("div");
+    content.classList.add("content");
+    screen.appendChild(content);
+  }
+  const pixels = document.querySelectorAll(".content");
+  draw(pixels);
 }
+createScreen(screenArea);
 
 const pixels = document.querySelectorAll(".content");
-pixels.forEach((pixel) => {
-  pixel.addEventListener("mouseover", () => {
-    pixel.classList.add("drawing");
-  }),
-    clearButton.addEventListener("transitionend", () => {
-      clearButton.classList.remove("clicked"), clearScreen(pixel, "drawing");
+function draw(elemName) {
+  elemName.forEach((pixel) => {
+    pixel.addEventListener("mouseover", () => {
+      pixel.classList.add("drawing");
     });
+  });
+}
+draw(pixels);
+
+clearButton.addEventListener("click", () => {
+  clearButton.classList.add("clicked"),
+    clearScreen(),
+    getGridArea(),
+    createScreen(screenArea);
 });
 
-function clearScreen(elem, elemClass) {
-  elem.classList.remove(elemClass);
+clearButton.addEventListener("transitionend", () => {
+  clearButton.classList.remove("clicked");
+});
+
+function clearScreen() {
+  pixels.forEach((pixel) => {
+    pixel.classList.remove("drawing");
+  });
+  while (screen.hasChildNodes()) {
+    screen.removeChild(screen.lastChild);
+  }
+}
+
+function getGridArea() {
+  do {
+    screenArea = parseInt(
+      prompt("Enter any number of squares but less than 100:"),
+      10
+    );
+  } while (isNaN(screenArea) || screenArea > 100 || screenArea < 1);
 }
